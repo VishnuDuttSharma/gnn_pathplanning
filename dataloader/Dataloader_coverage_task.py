0e7f15bdee52c29f7fef3d9074b4df3746277efc
Vishnu Dutt Sharma <vishnusharma619@gmail.com>
2021-03-17 21:17:57 -0400

    Created a class

--- dataloader/Dataloader_coverage_task.py
+++ dataloader/Dataloader_coverage_task.py
@@ -3,27 +3,69 @@ Data loader for coverage task
 """
 
 
-
+import logging
 import numpy as np
 import random
 import torch
 import pickle
+from torch.utils import data
+from torch.utils.data import DataLoader
+from glob import glob
 
+class GNNCoverageDataLoader:
+    def __init__(self, config):
+        self.config = config
+        self.logger = logging.getLogger("DecentralPlannerDataLoader Online Expert")
+        log_info = "Loading #{} Agent DATA from Path without OnlineTF .....".format(self.config.num_agents)
+        self.logger.info(log_info)
+    
+        if config.mode == "train":
+            trainlist = [self.config.data_root+f'data_{i+1}.pkl' for i in range(1, 60)]
+            train_set = GNNCoverageDataset(self.config, trainlist)
+            validlist = [self.config.data_root+f'data_{i+1}.pkl' for i in range(60, 80)]
+            valid_set = GNNCoverageDataset(self.config, validlist)
+
+            self.train_loader = DataLoader(train_set, batch_size=self.config.batch_size, shuffle=True,
+                                           num_workers=self.config.data_loader_workers,
+                                           pin_memory=self.config.pin_memory)
+            
+            self.valid_loader = DataLoader(valid_set, batch_size=self.config.valid_batch_size, shuffle=True,
+                                           num_workers=self.config.data_loader_workers,
+                                           pin_memory=self.config.pin_memory)
+        elif config.mode == "test":
+            testlist = [self.config.data_root+f'data_{i+1}.pkl' for i in range(80, 100)]
+            test_set = GNNCoverageDataset(self.config, testlist)
+            self.test_loader = DataLoader(test_set, batch_size=self.config.test_batch_size, shuffle=False,
+                                           num_workers=self.config.data_loader_workers,
+                                           pin_memory=self.config.pin_memory)
+
+        else:
+            raise Exception("Please specify in the json a specified mode in mode")
+
+    
 # Data shapes reference: https://github.com/proroklab/gnn_pathplanning/blob/master/dataloader/Dataloader_dcplocal_notTF_onlineExpert.py#L147-L149
-class GNNCoverageDataset(Dataset):
+class GNNCoverageDataset(data.Dataset):
     """Coverage task dataset, generated randomly and saved"""
 
-    def __init__(self, datafile):
+    def __init__(self, config, datafilelist):
         """
         Parameters
         ----------
-            datafile: Name of the dataset file
+            datafilelist: List of file of the dataset
             
         """
-        data = pickle.load(open(datafile, 'rb'))
-        self.features = data[0]
-        self.adj_mat  = data[1]
-        self.targets  = data[2]
+        numFeature = (config.tgt_feat + config.rbt_feat )
+        featlist, adjlist, tgtlist = [], [], []
+        for fl in datafilelist:
+            feat, adj, tgt = pickle.load(open(fl, 'rb'))
+            feat_reshaped = feat[:,:,:numFeature,:].reshape(feat.shape[0], feat.shape[1], numFeature*2)
+            featlist.append(feat_reshaped)
+            adjlist.append(adj)
+            tgtlist.append(tgt)
+
+        self.features = np.concatenate(featlist, axis=0)
+        self.adj_mat  = np.concatenate(adjlist, axis=0)
+        self.targets  = np.concatenate(tgtlist, axis=0)
 
     def __len__(self):
         return len(self.features)
@@ -53,12 +95,8 @@ def generate_data(data_size):
 
         feat_list.append(feat_vec)
         adj_list.append(adj_mat)
-        
-        action_one_hot = np.zeros((NUM_ROBOT, len(DIR_LIST)), dtype=np.uint8)
-        action_one_hot[np.arange(NUM_ROBOT), action_vec] = 1
-        label_list.append(action_one_hot)
+        label_list.append(action_vec)
     
-    return [np.array(feat_list), np.array(adj_list), np.array(label_list)]
-
+    return [np.array(feat_list).shape, np.array(adj_list).shape, np.array(label_list).shape]
 
  
